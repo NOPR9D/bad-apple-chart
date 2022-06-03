b2261a3d513fe91e8e2e91cd8c696dc85419e9a1
noprod <contact@boucham-amine.fr>
2022-06-03 21:53:01 +0200

    :hammer: : hello video to frames script

---
+++ scripts/videoToFrames.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+[ "$1" = "-h" -o "$1" = "--help" ] && echo "
+  This script is for VideoToFrames.
+
+  He extract all frames from a video file.
+
+  You can specify the file with file="", if no value the don't start.
+  You can specify the frames rate with fps="", if no value the scriptwill start will a default value of 30 fps.
+
+" && exit
+
+DEFAULT_FPS=30
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+    case "$KEY" in
+            file)              file=${VALUE} ;;
+            fps)    fps=${VALUE} ;;     
+            *)   
+    esac    
+done
+
+[ -z "$file" ] && echo "You can specity -h for help" &&  echo "You don't specify a file so we exit" && exit
+
+
+
+ffmpeg -skip_frame nokey -i ./assets/$file -vsync 0 -r ${fps:-${DEFAULT_FPS}} -f image2 thumbnails-%02d.jpeg
